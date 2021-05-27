7393b319a7d298645404356bbd3a8d7107e67d5a
elifcetinkaya <eliifctnk@gmail.com>
2021-05-27 16:59:24 +0300

    axios vs fetch, delete request

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import MovieList from "./MovieList";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
@@ -9,26 +10,63 @@ class App extends React.Component {
     searchQuery: "",
   };
 
+  // async componentDidMount() {
+  //   const baseURL = "http://localhost:3002/movies";
+  //   const response = await fetch(baseURL);
+  //   console.log(response);
+  //   const data = await response.json();
+  //   console.log(data);
+  //   this.setState({movies: data})
+  // }
+
   async componentDidMount() {
-    const baseURL = "http://localhost:3002/movies";
-    const response = await fetch(baseURL);
-    console.log(response);
-    const data = await response.json();
-    console.log(data);
-    this.setState({movies: data})
+    const response = await axios.get("http://localhost:3002/movies");
+    //console.log(response);
+    this.setState({ movies: response.data });
   }
 
-  deleteMovie = (movie) => {
-    const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
+  // deleteMovie = (movie) => {
+  //   const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
 
-    // this.setState({
-    //   movies: newMovieList,
-    // });
+  //   // this.setState({
+  //   //   movies: newMovieList,
+  //   // });
 
-    this.setState((state) => ({
-      movies: newMovieList,
-    }));
-  };
+  //   this.setState((state) => ({
+  //     movies: newMovieList,
+  //   }));
+  // };
+
+  // fetch api
+  // deleteMovie = async (movie) => {
+  //   const baseURL = `http://localhost:3002/movies/${movie.id}`;
+  //   await fetch(baseURL, {
+  //     method: "DELETE"
+  //   })
+  //   const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
+
+  //   // this.setState({
+  //   //   movies: newMovieList,
+  //   // });
+
+  //   this.setState((state) => ({
+  //     movies: newMovieList,
+  //   }));
+  // };
+
+    // axios api
+    deleteMovie = async (movie) => {
+      axios.delete(`http://localhost:3002/movies/${movie.id}`)
+      const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
+  
+      // this.setState({
+      //   movies: newMovieList,
+      // });
+  
+      this.setState((state) => ({
+        movies: newMovieList,
+      }));
+    };
 
   searchMovie = (event) => {
     // console.log(event.target.value)
