df82c9811cf6198c1360fdaf98e4d6f1317fbbe2
elifcetinkaya <eliifctnk@gmail.com>
2021-05-28 15:11:54 +0300

    real api connection

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import MovieList from "./MovieList";
 import axios from "axios";
+require("dotenv").config();
+
+console.log(process.env.REACT_APP_API_KEY);
 
 class App extends React.Component {
   state = {
@@ -10,32 +13,13 @@ class App extends React.Component {
     searchQuery: "",
   };
 
-  // async componentDidMount() {
-  //   const baseURL = "http://localhost:3002/movies";
-  //   const response = await fetch(baseURL);
-  //   console.log(response);
-  //   const data = await response.json();
-  //   console.log(data);
-  //   this.setState({movies: data})
-  // }
-
   async componentDidMount() {
-    const response = await axios.get("http://localhost:3002/movies");
-    //console.log(response);
-    this.setState({ movies: response.data });
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/list/7096805?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+    );
+    console.log(response.data.items);
+    this.setState({ movies: response.data.items });
   }
-
-  // deleteMovie = (movie) => {
-  //   const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
-
-  //   // this.setState({
-  //   //   movies: newMovieList,
-  //   // });
-
-  //   this.setState((state) => ({
-  //     movies: newMovieList,
-  //   }));
-  // };
 
   // fetch api
   // deleteMovie = async (movie) => {
@@ -54,19 +38,21 @@ class App extends React.Component {
   //   }));
   // };
 
-    // axios api
-    deleteMovie = async (movie) => {
-      axios.delete(`http://localhost:3002/movies/${movie.id}`)
-      const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
-  
-      // this.setState({
-      //   movies: newMovieList,
-      // });
-  
-      this.setState((state) => ({
-        movies: newMovieList,
-      }));
-    };
+  // axios api
+  deleteMovie = async (movie) => {
+    axios.post(
+      `https://api.themoviedb.org/3/list/7096805/remove_item?media_id=${movie.id}&session_id=${process.env.REACT_APP_SESSION_ID}&api_key=${process.env.REACT_APP_API_KEY}`
+    );
+    const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
+
+    // this.setState({
+    //   movies: newMovieList,
+    // });
+
+    this.setState((state) => ({
+      movies: newMovieList,
+    }));
+  };
 
   searchMovie = (event) => {
     // console.log(event.target.value)
@@ -78,7 +64,7 @@ class App extends React.Component {
   render() {
     let filteredMovies = this.state.movies.filter((movie) => {
       return (
-        movie.name
+        movie.title
           .toLowerCase()
           .indexOf(this.state.searchQuery.toLowerCase()) !== -1
       );
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,41 +1,39 @@
 import React from "react";
 
 const MovieList = (props) => {
-
   // function handleClick(event) {
   //   console.log(event.pageX );
   // }
 
-    return (
-      <div className="row">
-        {props.movies.map((movie) => (
-          <div className="col-lg-4" key={movie.id}>
-            <div className="card mb-4 shadow-sm">
-              <img
-                src={movie.imageURL}
-                className="card-img-top"
-                alt="Sample Movie"
-              />
-              <div className="card-body">
-                <h5 className="card-title">{movie.name}</h5>
-                <p className="card-text">{movie.overview}</p>
-                <div className="d-flex justify-content-between align-items-center">
-                  <button
-                    type="button"
-                    className="btn btn-md btn-outline-danger"
-                    onClick={(event)=> (props.deleteMovieProp(movie))}
-                  >
-                    Delete
-                  </button>
-                  <h2>{movie.rating}</h2>
-                </div>
+  return (
+    <div className="row">
+      {props.movies.map((movie) => (
+        <div className="col-lg-4" key={movie.id}>
+          <div className="card mb-4 shadow-sm">
+            <img
+              src={`https://www.themoviedb.org/t/p/w220_and_h330_face/${movie.poster_path}`}
+              className="card-img-top"
+              alt="Sample Movie"
+            />
+            <div className="card-body">
+              <h5 className="card-title">{movie.title}</h5>
+              <p className="card-text">{movie.overview}</p>
+              <div className="d-flex justify-content-between align-items-center">
+                <button
+                  type="button"
+                  className="btn btn-md btn-outline-danger"
+                  onClick={(event) => props.deleteMovieProp(movie)}
+                >
+                  Delete
+                </button>
+                <h2>{movie.vote_average}</h2>
               </div>
             </div>
           </div>
-        ))}
-      </div>
-    );
-
-}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default MovieList;
